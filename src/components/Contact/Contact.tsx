2dd6beb390d1c4e262fa78936a11522a3c752cd4
Nishantk2001 <nishant.k28101@gamil.com>
2025-04-01 15:40:16 +0530

    feat: Modify Contact Section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,33 +1,49 @@
 import styles from './contact.module.css';
+import { MdEmail } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
+
 
 const Contact = () => {
   return (
     <div className={styles.contactContainer}>
       <div className={styles.textContent}>
-        <h2 className={styles.sectionTitle}>Lorem ipsum dolor sit amet, consectetur</h2>
+        <h2 className={styles.sectionTitle}>Get In Touch</h2>
         
         <div className={styles.textColumns}>
           <div className={styles.column}>
-            <p>Suspendisse ipsum mauris,</p>
-            <p>vulputate vel ante eu</p>
-            <p>Proin non nibh non leo auctor isculis vitae vel est. Donec eget erat in</p>
-            <p>enim mattis gravida. Suspendisse lacinia eleifend mauris eget posuere.</p>
+            <p>I'd love to hear from you! Whether you have a project in mind, a question, or just want to say hello, feel free to reach out. Your ideas and inquiries are always welcome.</p>
           </div>
         </div>
 
         <div className={styles.contactInfo}>
           <div className={styles.infoBlock}>
-            <h3>Postal Address</h3>
-            <p>1870 sed consequat risus, at vestibulum lacus.</p>
-          </div>
-          <div className={styles.infoBlock}>
-            <h3>General Enquiries</h3>
+            <div className={styles.iconBox}>
+          <MdEmail color='white' size={25}/>
+            <h3>Email Address</h3>
+            </div>
             <p>no-reply@yourdomain.com / sales@yourdomain.com</p>
           </div>
+          <div className={styles.infoDivider}></div>
+
           <div className={styles.infoBlock}>
-            <h3>Business Phone</h3>
+          <div className={styles.iconBox}>
+          <FaPhoneAlt color='white' size={25}/>
+            <h3>Businnes Phone</h3>
+            </div>
             <p>773-222-4444 / 7722-545-8888</p>
           </div>
+          <div className={styles.infoDivider}></div>
+
+          <div className={styles.infoBlock}>
+          <div className={styles.iconBox}>
+          <FaLocationDot color='white' size={25}/>
+            <h3>Address</h3>
+            </div>
+            <p>1870 sed consequat risus, at vestibulum lacus.</p>
+          </div>
+          
         </div>
       </div>
 
@@ -44,7 +60,7 @@ const Contact = () => {
             <input type="tel" placeholder="Phone Number" />
           </div>
           <div className={styles.formGroup}>
-            <textarea placeholder="Your Message" rows={5} required></textarea>
+            <textarea placeholder="Your Message" rows={7} required></textarea>
           </div>
           <button type="submit" className={styles.submitButton}>Send Message</button>
         </form>
